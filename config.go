d84ae62ef7b2eb76eb88e8f14b044ff8444dac74
oleksgol <alex.goldobin@gmail.com>
2015-11-18 17:29:35 +0100

    Initial commit. Firts implementation of cluster and node config.

---
+++ config.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+type ClusterConf struct {
+	ListenHost   string
+	Ports        []int
+	ReplicaCount int
+}
+
+type RedisNodeConf struct {
+	ListenHost  string
+	ListenPort  int
+	Persistence bool
+	DataDir     string
+	PidFile     string
+	LogFile     string
+}
+
+func LoadClusterConf(fileName string) (r *ClusterConf, err error) {
+
+	data, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	r = &ClusterConf{}
+
+	err = yaml.Unmarshal(data, r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func SaveClusterConf(fileName string, conf *ClusterConf) (err error) {
+
+	data, err := yaml.Marshal(&conf)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(fileName, data, 0644)
+}
+
+func SaveRedisConf(fileName string, conf *RedisNodeConf) (err error) {
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(f)
+
+	w.WriteString("daemonize yes\n")
+	w.WriteString("cluster-enabled yes\n")
+	w.WriteString("loglevel notice\n")
+
+	if len(conf.ListenHost) > 0 {
+		fmt.Fprintf(w, "bind %s\n", conf.ListenHost)
+	}
+
+	if conf.ListenPort > 0 {
+		fmt.Fprintf(w, "port %d\n", conf.ListenPort)
+	}
+
+	if len(conf.PidFile) > 0 {
+		fmt.Fprintf(w, "pidfile %s\n", conf.PidFile)
+	}
+
+	if len(conf.LogFile) > 0 {
+		fmt.Fprintf(w, "logfile %s\n", conf.LogFile)
+	}
+
+	if conf.Persistence {
+		w.WriteString("appendonly yes\n")
+		fmt.Fprintf(w, "dir %s\n", conf.DataDir)
+	} else {
+		w.WriteString("appendonly no\n")
+		w.WriteString("save \"\"\n")
+	}
+
+	w.Flush()
+
+	return nil
+}
---
+++ config_test.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"reflect"
+	"regexp"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestSaveLoadClusterConf(t *testing.T) {
+
+	cases := []ClusterConf{
+		ClusterConf{
+			ListenHost:   "127.0.0.1",
+			Ports:        []int{7501, 7502, 7503, 7504, 7505, 7506},
+			ReplicaCount: 1,
+		},
+		ClusterConf{
+			ListenHost:   "loaclhost",
+			Ports:        []int{7501, 7502},
+			ReplicaCount: 10,
+		},
+	}
+
+	tmpdir, err := ioutil.TempDir("", "rcm_cluster_conf_test")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.RemoveAll(tmpdir)
+
+	for _, conf := range cases {
+
+		fname := tmpdir + "/" + randStringRunes(6) + ".yml"
+
+		err := SaveClusterConf(fname, &conf)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		loadedConf, err := LoadClusterConf(fname)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if reflect.DeepEqual(loadedConf, conf) {
+			t.Errorf("Expected %v but got %v", conf, *loadedConf)
+		}
+	}
+}
+
+func TestSaveNodeConf(t *testing.T) {
+	cases := []RedisNodeConf{
+		RedisNodeConf{
+			ListenHost:  "127.0.0.1",
+			ListenPort:  6379,
+			Persistence: true,
+			DataDir:     "/tmp",
+			LogFile:     "/tmp/redis.log",
+		},
+		RedisNodeConf{
+			ListenPort:  6379,
+			Persistence: true,
+			DataDir:     "/tmp",
+			LogFile:     "/tmp/redis.log",
+		},
+	}
+
+	tmpdir, err := ioutil.TempDir("", "rcm_cluster_conf_test")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.RemoveAll(tmpdir)
+
+	for _, conf := range cases {
+
+		fname := tmpdir + "/" + randStringRunes(6) + ".conf"
+
+		SaveRedisConf(fname, &conf)
+
+		data, err := ioutil.ReadFile(fname)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		dataStr := string(data)
+
+		checkConfigParam := func(paramName string, expected string) {
+
+			re := redisConfRegExps[paramName]
+
+			if re == nil {
+				panic(fmt.Sprintf("Ther is no pattern to extract value for '%s' parameter form redis config", paramName))
+			}
+
+			matches := re.FindStringSubmatch(dataStr)
+
+			if expected == "" {
+				if len(matches) != 0 {
+					t.Errorf("'%s' record SHOULD NOT be present in the redis.conf file", paramName)
+				}
+			} else {
+				if len(matches) != 2 {
+					t.Errorf("'%s' record SHOULD be present in the redis.conf file", paramName)
+				} else {
+					listenHost := matches[1]
+					if matches[1] != expected {
+						t.Errorf("Expected %v but got %v for '%s' parameter", expected, listenHost, paramName)
+					}
+				}
+			}
+		}
+
+		var expectedListenPortStr string
+
+		if conf.ListenPort > 0 {
+			expectedListenPortStr = strconv.Itoa(conf.ListenPort)
+		} else {
+			expectedListenPortStr = ""
+		}
+
+		checkConfigParam("bind", conf.ListenHost)
+		checkConfigParam("port", expectedListenPortStr)
+
+		checkConfigParam("dir", conf.DataDir)
+	}
+}
+
+// Supporting code
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+var redisConfRegExps = map[string]*regexp.Regexp{
+	"bind": regexp.MustCompile(`bind\s*([0-9\w\._-]+)\s*\n`),
+	"port": regexp.MustCompile(`port\s*([0-9]+)\s*\n`),
+	"dir":  regexp.MustCompile(`dir\s*([0-9\w/\._-]+)\s*\n`),
+}
